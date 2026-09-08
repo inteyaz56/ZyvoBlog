1907674b0515614ede4fe0300d5be2e300339cbc
Inteyaz <inteyazansari322@gmail.com>
2026-09-08 17:47:36 +0530

    implemented logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Session;
 
 class UserController extends Controller
 {
@@ -53,6 +54,14 @@ class UserController extends Controller
         if (Auth::attempt($credentials)) {
             return redirect("/");
         }
+
+        return redirect("/users/login");
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        Auth::logout();
 
         return redirect("/users/login");
     }
